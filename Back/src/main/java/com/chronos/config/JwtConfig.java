8affd6a6e9fe599220057b7df04c07316ca291e8
Jenrimark <2303532728@qq.com>
2025-12-28 02:52:37 +0800

    净化目录

--- Back/src/main/java/com/chronos/config/JwtConfig.java
+++
@@ -1,9 +0,0 @@
-package com.chronos.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JwtConfig {
-    // JWT配置类，后续可以扩展JWT相关功能
-}
-
--- Back/src/main/java/com/chronos/service/AIService.java
+++
@@ -1,9 +0,0 @@
-package com.chronos.service;
-
-import java.util.Map;
-
-public interface AIService {
-    Map<String, Object> recognizeBill(String description, Double amount);
-    Map<String, Object> extractBillInfo(String text);
-}
-
